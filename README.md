a67dd66e22bd4696789c270594917435dd120411
dmills <dmills@iastate.edu>
2015-02-14 20:03:23 -0600

    updated readme and examples

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # whale.js
 Whale is a tiny library which provides strong MVC tools and components.
 
+* Classical inheritance
+* Dependency injection
+* Factory and Service patterns (built off dependency injection)
+* Events (dispatchers and listeners)
+* Model, View, Controller classes (soon)
 
-### Dispatchers and Listeners
+## Examples
+Check out the examples to see how the different patterns are implemented
+
+### output for parent-child example (classical inheritance)
+* "hello my name is Bob"
+* "Arrr matey, my name is Pegleg. My weapon is a cannon"
+* "Arrr matey, my name is Gandalf. My weapon is a Sword"
+* "and I'm the captain!"
+
+### output for harbor example (service and factory)
+* "A green boat arrives the harbor"
+* "A blue boat arrives the harbor"
+* "A yellow boat arrives the harbor"
+* "The harbor has 3 boats"
+* "* a green boat"
+* "* a blue boat"
+* "* a yellow boat"
+
+### output for events example (dispatchers and listeners)
 * "Jim takes a drink as the bard starts to sing"
 * "Jim hums along while the bard continues to sing"
 * "the bard is singing..."
@@ -15,4 +38,3 @@ Whale is a tiny library which provides strong MVC tools and components.
 * "the bard is singing..."
 * "the bard is singing..."
 * "the bard is singing..."
-
--- example/events/events.js
+++ example/events/events.js
@@ -5,41 +5,59 @@ var Bard = whale.Dispatcher.extend ({
 });
 
 var Traveller = whale.Listener.extend ({
-  initialize: function() {
-    this.blood = 20;
-  },
-
+  /* Traveller has a construcor which takes in a name and a Bard */
   construct: function (name, bard) {
     this.name = name;
     this.drinks = 0;
+    /* listenOnce will only all the function to trigger one time
+     * this line says: "call the function this.drink once the bard
+     * triggers 'singing' event" */
     this.listenOnce (bard, 'singing', this.drink);
   },
 
   drink: function (bard) {
     this.drinks++;
     console.log (this.name + ' takes a drink as the bard starts to sing');
+    /* listen tells this object to call 'this.hum' whenever bard triggers 'singing' */
     this.listen (bard, 'singing', this.hum);
   },
+
 
   hum: function (bard) {
     this.drinks++;
     console.log (this.name + ' hums along while the bard continues to sing');
     if (this.drinks > 4) {
       console.log (this.name + ' has had enough to drink and stops listening to the bard');
-      this.stopListening(bard);
+      /* stop listening to all events from the bard. note this method can be more
+       * specific:
+       *  this.stopListening(bard, 'singing') and:
+       *  this.stopListening (bard, 'singing', this.hum)
+       */
+      this.stopListening (bard);
     }
   }
 });
 
+/* create new Bard object */
 var bard = new Bard;
+
+/* whenever the bard sings, print out some stuff.
+ * note how this event is attached to the bard
+ * (instead of the listening function,
+ * like obj.listen()) */
 bard.when('singing', function() {
   console.log ('the bard is singing...');
 });
+
+/* call the sing() method, which triggers the 'singing' event */
 bard.sing();
 
 
+/* Create an instance of traveller called Bob who listens to bard */
 var bob = new Traveller ('Bob', bard);
 
+/* let the bard sing */
+bard.sing();
 bard.sing();
 bard.sing();
 bard.sing();
@@ -47,7 +65,6 @@ bard.sing();
 bard.sing();
 bard.sing();
 
-bard.sing();
 /*
   OUTPUT
 
