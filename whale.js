b419fbac721be7e96a87f1965597ebff7487ab6e
dmills <dmills@iastate.edu>
2015-04-17 01:17:52 -0500

    added microtemplating based on underscore (which is based on John Resigs approach)

--- whale.js
+++ whale.js
@@ -934,4 +934,71 @@
 
   });
 
+  whale.util = {};
+
+  var entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;'
+  };
+
+  var escapes = {
+    "'":      "'",
+    '\\':     '\\',
+    '\r':     'r',
+    '\n':     'n',
+    '\u2028': 'u2028',
+    '\u2029': 'u2029'
+  };
+  var tmpReg = /\{\{-(.+?)\}\}|\{\{=(.+?)\}\}|\{\{(.+?)\}\}|$/g;
+  var escReg = /\\|'|\r|\n|\u2028|\u2029/g;
+
+  var escChar = function(match) {
+    return '\\' + escapes[match];
+  }
+
+  whale.util.escape = function (text) {
+      return String(text).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+      });
+  },
+
+  whale.util.template = function (text) {
+    var index = 0;
+    var source = "__p+='";
+    text.replace (tmpReg, function(match, escape, interpolate, evaluate, offset) {
+      source += text.slice (index, offset).replace (escReg, escChar);
+      index = offset + match.length;
+
+      if (escape) {
+        source += "'+\n((__t=(" + escape + "))==null?'':whale.util.escape(__t))+\n'";
+      } else if (interpolate) {
+        source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+      } else if (evaluate) {
+        source += "';\n" + evaluate + "\n__p+='";
+      }
+
+      return match;
+    });
+
+    source += "';\n";
+    source = 'with(obj||{}){\n' + source + '}\n';
+    source = "var __t,__p='',__j=Array.prototype.join," +
+      "print=function(){__p+=__j.call(arguments,'');};\n" +
+      source + 'return __p;\n';
+
+    var render = new Function('obj', 'whale', source);
+
+    var template = function (data) {
+      return render.call(this, data, whale);
+    };
+
+    template.source = 'function(obj){\n' + source + '}';
+    return template;
+  }
+
 }.call(this));
